c73a5d3c32fda292d0dedfd8ff2b551dca1a7179
aajfranklin <aajfranklin@gmail.com>
2019-07-10 21:05:23 +0100

    feat: highlight names loaded after initial page load

--- content/content.js
+++ content/content.js
@@ -1,7 +1,7 @@
-const searchDocumentBody = (playerNames) => {
-  const bodyText = document.body.textContent;
+const searchTextContent = (rootNode, playerNames) => {
+  const nodeText = rootNode.textContent;
   const ac = new AhoCorasick(playerNames);
-  return ac.search(bodyText);
+  return ac.search(nodeText);
 };
 
 const getNextResult = (results) => {
@@ -17,19 +17,21 @@ const getNextResult = (results) => {
   return undefined;
 };
 
-const locateAndFormatResults = (results) => {
-  const treeWalker = document.createTreeWalker(document.body, 4);
+const locateAndFormatResults = (rootNode, results) => {
+  const treeWalker = document.createTreeWalker(rootNode, 4);
   let currentTextIndex = 0;
   let nextResult = getNextResult(results);
 
   while (nextResult !== undefined) {
-    // traverse node tree and locate node containing next result
-    const currentNode = treeWalker.nextNode();
+    // traverse node tree and locate text node containing next result
+    const currentNode = treeWalker.currentNode.nodeName === '#text'
+                          ? treeWalker.currentNode
+                          : treeWalker.nextNode();
     const nodeTextLength = currentNode.textContent.length;
-    const nodeIncludesNextMatch = currentTextIndex + nodeTextLength >= nextResult.index;
+    const nodeIncludesNextResult = currentTextIndex + nodeTextLength >= nextResult.index;
 
-    if (nodeIncludesNextMatch) {
-      // do not reformat text nodes within script and style elements, to preserve DOM integrity
+    if (nodeIncludesNextResult) {
+      // do not reformat text nodes within script and style elements, these are not displayed to the user
       const parentNodeName = currentNode.parentNode.nodeName;
       const parentNodeIsValid = parentNodeName !== 'SCRIPT' && parentNodeName !== 'STYLE';
 
@@ -38,10 +40,10 @@ const locateAndFormatResults = (results) => {
       }
 
       nextResult = getNextResult(results);
-      treeWalker.previousNode();
 
     } else {
       currentTextIndex += nodeTextLength;
+      treeWalker.nextNode();
     }
   }
 };
@@ -62,10 +64,35 @@ const highlightResult = (result, node, currentTextIndex) => {
   range.surroundContents(wrapper);
 };
 
-// consider moving to background script, to run only once
+const observeMutations = (playerNames) => {
+  const observer = new MutationObserver(function (mutations) {
+    for (let i = 0; i < mutations.length; i++) {
+      const addedNodes = mutations[i].addedNodes;
+      if (addedNodes.length > 0) {
+        const mutationRootNode = addedNodes[0];
+        const results = searchTextContent(mutationRootNode, playerNames);
+        if (results.length > 0) {
+          observer.disconnect();
+          locateAndFormatResults(mutationRootNode, results);
+          observer.observe(document.body, { childList: true, subtree: true });
+        }
+      }
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+};
+
+// consider moving to background script, or fetching and caching in local storage on first use
 const playerNames = playersById.map((player) => {
   return player.name;
 });
 
-const results = searchDocumentBody(playerNames);
-locateAndFormatResults(results);
+const body = document.body;
+const initialResults = searchTextContent(body, playerNames);
+
+if (initialResults.length > 0) {
+  locateAndFormatResults(body, initialResults);
+}
+
+observeMutations(playerNames);
