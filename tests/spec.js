3837bd995f2c6a31f51e4185b8949d26be5f22c6
aajfranklin <aajfranklin@gmail.com>
2019-08-14 17:43:48 +0100

    test: add unit tests for background script requests

--- tests/spec.js
+++ tests/spec.js
@@ -1,8 +1,108 @@
 
+describe('Utils', () => {
+
+  let testUtils;
+
+  before(() => {
+    testUtils = new Utils();
+  });
+
+  describe('checkPlayers', () => {
+
+    let saveToChromeStorageStub;
+    let backgroundScriptRequestStub;
+    const fetchedPlayers = 'mockFetchedPlayers';
+    const cachedPlayers = {players: 'mockCachedPlayers'};
+
+    before(() => {
+      saveToChromeStorageStub = sinon.stub(testUtils, 'saveToChromeStorage');
+      backgroundScriptRequestStub = sinon.stub(testUtils, 'backgroundScriptRequest');
+      backgroundScriptRequestStub.resolves(fetchedPlayers);
+    });
+
+    afterEach(() => {
+      saveToChromeStorageStub.resetHistory();
+      backgroundScriptRequestStub.resetHistory();
+    });
+
+    after(() => {
+      saveToChromeStorageStub.restore();
+      backgroundScriptRequestStub.restore();
+    });
+
+    it('should return players fetched from storage if they are present', () => {
+      return testUtils.checkPlayers(cachedPlayers)
+        .then((response) => {
+          expect(backgroundScriptRequestStub.notCalled).to.equal(true);
+          expect(saveToChromeStorageStub.notCalled).to.equal(true);
+          expect(response).to.equal('mockCachedPlayers')
+        })
+    });
+
+    it('should fetch and store players if they are not in local chrome storage', () => {
+      return testUtils.checkPlayers(undefined)
+        .then((response) => {
+          expect(backgroundScriptRequestStub.withArgs({message: 'fetchPlayers'}).calledOnce).to.equal(true);
+          expect(saveToChromeStorageStub.withArgs('players', fetchedPlayers).calledOnce).to.equal(true);
+          expect(response).to.equal(fetchedPlayers);
+        })
+    });
+
+  });
+
+  describe('backgroundScriptRequest', () => {
+
+    let sendMessageStub;
+    let request = {message: 'testRequest'};
+    let response;
+
+    before(() => {
+      response = [null, 'res'];
+      sendMessageStub = sinon.stub(chrome.runtime, 'sendMessage');
+      sendMessageStub.callsArgWith(1, response);
+    });
+
+    afterEach(() => {
+      sendMessageStub.resetHistory();
+    });
+
+    after(() => {
+      sendMessageStub.restore();
+    });
+
+    it('should send a message to chrome runtime with passed in request', () => {
+      return testUtils.backgroundScriptRequest(request)
+        .then(() => {
+          expect(sendMessageStub.calledOnce).to.equal(true);
+          expect(sendMessageStub.withArgs(request).calledOnce).to.equal(true);
+        });
+    });
+
+    it('should resolve response if chrome returns no error', () => {
+      return testUtils.backgroundScriptRequest(request)
+        .then((res) => {
+          expect(res).to.equal('res');
+        });
+    });
+
+    it('should reject if chrome returns an error', () => {
+      response = ['err', null];
+      sendMessageStub.callsArgWith(1, response);
+
+      return testUtils.backgroundScriptRequest(request)
+        .catch((err) => {
+          expect(err).to.equal('err');
+        })
+    });
+
+  });
+
+});
+
 describe('ClickAndRoll', () => {
+
   let players;
   let testClickAndRoll;
-  let testUtils;
 
   before(() => {
     players = [
@@ -10,49 +110,6 @@ describe('ClickAndRoll', () => {
       {id: 2, name: 'LeBron James'},
     ];
     testClickAndRoll = new ClickAndRoll(players);
-    testUtils = new Utils();
-  });
-
-  describe('Set up and start stage', () => {
-
-    describe('checkPlayers', () => {
-
-      let saveToChromeStorageStub;
-      let backgroundScriptRequestStub;
-      const fetchedPlayers = 'mockFetchedPlayers';
-      const cachedPlayers = {players: 'mockCachedPlayers'};
-
-      beforeEach(() => {
-        saveToChromeStorageStub = sinon.stub(testUtils, 'saveToChromeStorage');
-        backgroundScriptRequestStub = sinon.stub(testUtils, 'backgroundScriptRequest');
-        backgroundScriptRequestStub.resolves(fetchedPlayers);
-      });
-
-      afterEach(() => {
-        saveToChromeStorageStub.restore();
-        backgroundScriptRequestStub.restore();
-      });
-
-      it('should return players fetched from storage if they are present', () => {
-        return testUtils.checkPlayers(cachedPlayers)
-          .then((response) => {
-            expect(backgroundScriptRequestStub.notCalled).to.equal(true);
-            expect(saveToChromeStorageStub.notCalled).to.equal(true);
-            expect(response).to.equal('mockCachedPlayers')
-          })
-      });
-
-      it('should fetch and store players if they are not in local chrome storage', () => {
-        return testUtils.checkPlayers(undefined)
-          .then((response) => {
-            expect(backgroundScriptRequestStub.withArgs({message: 'fetchPlayers'}).calledOnce).to.equal(true);
-            expect(saveToChromeStorageStub.withArgs('players', fetchedPlayers).calledOnce).to.equal(true);
-            expect(response).to.equal(fetchedPlayers);
-          })
-      });
-
-    });
-
   });
 
   describe('Find and format matches stage', () => {
