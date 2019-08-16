15c3ffeeeeb5ad29f34242c3e950831ff762dd0b
aajfranklin <aajfranklin@gmail.com>
2019-08-16 16:55:25 +0100

    wip: refactor result search and add more tests

--- content/resultSearch.js
+++ content/resultSearch.js
@@ -6,7 +6,7 @@ function ResultSearch() {
 
   this.searchRootNode = (rootNode) => {
     const hits = this.searchText(rootNode.textContent);
-    const filteredHits = this.filterSubStrings(hits);
+    const filteredHits = this.filterOverlappingStrings(hits);
     return this.mapHitsToResultNodes(rootNode, filteredHits);
   };
 
@@ -16,55 +16,43 @@ function ResultSearch() {
       return {
         start: rawHit[0] - rawHit[1][0].length + 1,
         end: rawHit[0],
-        name: rawHit[1][0]
+        text: rawHit[1][0]
       }
     });
   };
 
-  this.filterSubStrings = (hits) => {
+  this.filterOverlappingStrings = (hits) => {
     return hits.filter((hit, i) => {
       const nextHit = hits[i + 1];
       if (nextHit) {
-        const hitEnd = this.getHitBounds(hit).end;
-        const nextHitStart = this.getHitBounds(nextHit).start;
-
-        return (hitEnd < nextHitStart);
+        return (hit.end < nextHit.start);
       }
       return true;
     });
   };
 
-  this.getHitBounds = (hit) => {
-    const end = hit[0];
-    const start = end - hit[1][0].length + 1;
-    return {
-      start,
-      end
-    }
-  };
-
   this.mapHitsToResultNodes = (rootNode, hits) => {
     const treeWalker = document.createTreeWalker(rootNode, 4);
     let currentTextIndex = 0;
-    let nextHit = this.getNextHit(hits);
-    let currentNode = rootNode;
+    let nextHit = hits.shift();
+    let currentNode = treeWalker.currentNode;
     const resultNodes = [];
 
-    while (nextHit !== null && currentNode !== null) {
+    while (nextHit !== undefined && currentNode !== null) {
       // traverse node tree and locate text node containing next hit
-      if (treeWalker.currentNode.nodeName !== '#text') {
+      if (currentNode.nodeName !== '#text') {
         currentNode = treeWalker.nextNode();
         continue;
       }
       const nodeTextLength = currentNode.textContent.length;
-      const nodeIncludesNextHit = currentTextIndex + nodeTextLength >= nextHit.index;
+      const nodeIncludesNextHit = currentTextIndex + nodeTextLength >= nextHit.end;
 
       if (nodeIncludesNextHit) {
         if (this.parentNodeIsValid(currentNode)) {
           const resultNode = this.createResultNode(nextHit, currentNode, currentTextIndex);
           if (resultNode !== null) resultNodes.push(resultNode);
         }
-        nextHit = this.getNextHit(hits);
+        nextHit = hits.shift();
       } else {
         currentTextIndex += nodeTextLength;
         currentNode = treeWalker.nextNode();
@@ -72,17 +60,6 @@ function ResultSearch() {
     }
 
     return resultNodes;
-  };
-
-  this.getNextHit = (hits) => {
-    const rawHit = hits.shift();
-    if (rawHit !== undefined) {
-      return {
-        index: rawHit[0],
-        name: rawHit[1][0]
-      }
-    }
-    return null;
   };
 
   this.parentNodeIsValid = (currentNode) => {
@@ -94,24 +71,14 @@ function ResultSearch() {
   };
 
   this.createResultNode = (hit, hitNode, currentTextIndex) => {
-    const hitEndOffset = hit.index - currentTextIndex + 1;
-    const hitStartOffset = hitEndOffset - hit.name.length;
-
-    if (hitStartOffset < 0) {
-      /*
-      Hit is not contained wholly by a single node:
-
-      -  <div>LeBron</div><div>James</div>
-      -  <div>LeBron <b>James</b></div>
-
-      Or similar
-       */
+    // ignore hits split across multiple nodes
+    if (hit.start < currentTextIndex) {
       return null;
     }
 
     const range = document.createRange();
-    range.setStart(hitNode, hitStartOffset);
-    range.setEnd(hitNode, hitEndOffset);
+    range.setStart(hitNode, hit.start - currentTextIndex);
+    range.setEnd(hitNode, hit.end - currentTextIndex + 1);
 
     const wrapper = document.createElement('span');
     range.surroundContents(wrapper);
--- tests/spec.js
+++ tests/spec.js
@@ -144,12 +144,10 @@ describe('Content Scripts', () => {
 
   describe('ResultSearch', () => {
 
-    let body;
     let searchStrings;
     let testResultSearch;
 
     before(() => {
-      body = document.createElement('body');
       searchStrings = ['LeBron James', 'Michael Jordan'];
       testResultSearch = new ResultSearch();
 
@@ -169,64 +167,84 @@ describe('Content Scripts', () => {
       it('should return start index, end index, and name of matched players', () => {
         const text = 'LeBron James Michael Jordan';
         const expectedResult = [
-          {start: 0, end: 11, name: 'LeBron James'},
-          {start: 13, end: 26, name: 'Michael Jordan'}
+          {start: 0, end: 11, text: 'LeBron James'},
+          {start: 13, end: 26, text: 'Michael Jordan'}
         ];
         expect(testResultSearch.searchText(text)).to.deep.equal(expectedResult);
       });
 
     });
 
-    describe('locateAndFormatResults', () => {
+    describe('filterOverlappingStrings', () => {
 
-      let createTreeWalkerStub;
-      let nextResultStub;
-      let mockTreeWalker = {
-        currentNode: {
-          nodeName: null,
-        },
-        nextNode: null
-      };
+      it('should remove sub strings', () => {
+        const hits = [
+          {start: 2, end: 14, text: 'Marcus Cousin'},
+          {start: 0, end: 15, text: 'DeMarcus Cousins'}
+        ];
 
-      before(() => {
-        createTreeWalkerStub = sinon.stub(document, 'createTreeWalker');
-        createTreeWalkerStub.returns(mockTreeWalker);
-        nextResultStub = sinon.stub(testClickAndRoll, 'getNextHit');
+        const filteredHits = testResultSearch.filterOverlappingStrings(hits);
+        expect(filteredHits.length === 1);
+        expect(filteredHits[0]).to.deep.equal(hits[1]);
       });
 
-      beforeEach(() => {
-        nextResultStub.returns('someResult');
+      it('should remove partly overlapping strings', () => {
+        const hits = [
+          {start: 0, end: 11, text: 'LeBron James'},
+          {start: 7, end: 18, text: 'James Harden'}
+        ];
+
+        const filteredHits = testResultSearch.filterOverlappingStrings(hits);
+        expect(filteredHits.length === 1);
+        expect(filteredHits[0]).to.deep.equal(hits[1]);
+      });
+
+    });
+
+    describe('mapHitsToResultNodes', () => {
+
+      let createTreeWalkerStub;
+      let hits;
+      let nextHitStub;
+      let mockTreeWalker;
+
+      before(() => {
+        mockTreeWalker = {};
+        createTreeWalkerStub = sinon.stub(document, 'createTreeWalker');
+        createTreeWalkerStub.returns(mockTreeWalker);
+        hits = [];
+        nextHitStub = sinon.stub(hits, 'shift');
       });
 
       afterEach(() => {
         createTreeWalkerStub.resetHistory();
-        nextResultStub.resetHistory();
+        nextHitStub.resetHistory();
       });
 
       after(() => {
         createTreeWalkerStub.restore();
-        nextResultStub.restore();
+        nextHitStub.restore();
       });
 
       it('should stop when there are no more results', () => {
-        nextResultStub.returns(null);
-        testClickAndRoll.locateAndFormatResults(body, []);
-        expect(nextResultStub.calledOnce).to.equal(true);
-        expect(nextResultStub.withArgs([]).calledOnce).to.equal(true);
+        nextHitStub.returns(undefined);
+        testResultSearch.mapHitsToResultNodes('', hits);
+        expect(nextHitStub.calledOnce).to.equal(true);
       });
 
       it('should stop if the current node is null', () => {
-        testClickAndRoll.locateAndFormatResults(null, []);
-        expect(nextResultStub.calledOnce).to.equal(true);
-        expect(nextResultStub.withArgs([]).calledOnce).to.equal(true);
+        mockTreeWalker.currentNode = null;
+        testResultSearch.mapHitsToResultNodes('', hits);
+        expect(nextHitStub.calledOnce).to.equal(true);
       });
 
       it('should skip non-text nodes', () => {
-        mockTreeWalker.currentNode.nodeName = 'notText';
+        nextHitStub.returns('someHit');
+        mockTreeWalker.currentNode = {nodeName: 'notText'} ;
         mockTreeWalker.nextNode = () => null;
         const nextNodeSpy = sinon.spy(mockTreeWalker, 'nextNode');
 
-        testClickAndRoll.locateAndFormatResults(body, []);
+        testResultSearch.mapHitsToResultNodes('', hits);
         expect(nextNodeSpy.calledOnce).to.equal(true);
 
         mockTreeWalker.currentNode.nodeName = null;
@@ -236,56 +254,81 @@ describe('Content Scripts', () => {
 
       describe('if the current node does not include a result', () => {
 
+        let createResultNodeStub;
+        let hit;
         let nextNodeSpy;
 
         before(() => {
-          mockTreeWalker.nextNode = () => null;
           mockTreeWalker.currentNode.nodeName = '#text';
-          nextNodeSpy = sinon.spy(mockTreeWalker, 'nextNode');
-          nextResultStub.onCall(0).returns({index: 11, name: 'LeBron James'});
+          mockTreeWalker.currentNode.textContent = 'test';
+
+          createResultNodeStub = sinon.stub(testResultSearch, 'createResultNode');
+          hit = {start: 4, end: 15, text: 'LeBron James'};
+          nextHitStub.onCall(0).returns(hit);
+          nextHitStub.onCall(1).returns(undefined);
         });
 
         afterEach(() => {
-          nextNodeSpy.resetHistory();
+          createResultNodeStub.resetHistory();
         });
 
         after(() => {
           mockTreeWalker.nextNode = null;
           mockTreeWalker.currentNode.nodeName = null;
           mockTreeWalker.currentNode.textContent = null;
-          nextNodeSpy.restore();
+          createResultNodeStub.restore();
         });
 
         it('should progress to the next node', () => {
-          body.innerHTML = 'test';
-          testClickAndRoll.locateAndFormatResults(body, []);
+          mockTreeWalker.nextNode = () => null;
+          nextNodeSpy = sinon.spy(mockTreeWalker, 'nextNode');
+
+          testResultSearch.mapHitsToResultNodes('', hits);
           expect(nextNodeSpy.calledOnce).to.equal(true);
+          expect(createResultNodeStub.notCalled).to.equal(true);
+
+          nextNodeSpy.restore();
+        });
+
+        it('should increase the current text index by the node text length', () => {
+          const secondNode = {
+            nodeName: '#text',
+            textContent: 'LeBron James',
+          };
+          mockTreeWalker.nextNode = () => secondNode;
+          nextNodeSpy = sinon.spy(mockTreeWalker, 'nextNode');
+
+          testResultSearch.mapHitsToResultNodes('', hits);
+          expect(nextNodeSpy.calledOnce).to.equal(true);
+          expect(createResultNodeStub.calledOnce).to.equal(true);
+          expect(createResultNodeStub.withArgs(hit, secondNode, 4).calledOnce).to.equal(true);
         });
 
       });
 
       describe('if the current node includes a result', () => {
 
-        let highlightResultStub;
+        let createResultNodeStub;
+        let hit;
         let parentNodeIsValidStub;
 
         before(() => {
-          body.innerHTML = 'LeBron James';
-          highlightResultStub = sinon.stub(testClickAndRoll, 'highlightResult');
+          createResultNodeStub = sinon.stub(testResultSearch, 'createResultNode');
+          hit = {start: 0, end: 11, text: 'LeBron James'};
           mockTreeWalker.currentNode.nodeName = '#text';
           mockTreeWalker.currentNode.textContent = 'LeBron James';
-          nextResultStub.onCall(0).returns({index: 11, name: 'LeBron James'});
-          nextResultStub.onCall(1).returns(null);
-          parentNodeIsValidStub = sinon.stub(testClickAndRoll, 'parentNodeIsValid');
+          nextHitStub.onCall(0).returns(hit);
+          nextHitStub.onCall(1).returns(undefined);
+          parentNodeIsValidStub = sinon.stub(testResultSearch, 'parentNodeIsValid');
         });
 
         afterEach(() => {
-          highlightResultStub.resetHistory();
+          createResultNodeStub.resetHistory();
           parentNodeIsValidStub.resetHistory();
         });
 
         after(() => {
-          highlightResultStub.restore();
+          createResultNodeStub.restore();
           mockTreeWalker.currentNode.nodeName = null;
           mockTreeWalker.currentNode.textContent = null;
           parentNodeIsValidStub.restore();
@@ -293,24 +336,24 @@ describe('Content Scripts', () => {
 
         it('should check parent node validity', () => {
           parentNodeIsValidStub.returns(false);
-          testClickAndRoll.locateAndFormatResults(body, []);
+          testResultSearch.mapHitsToResultNodes('', hits);
           expect(parentNodeIsValidStub.calledOnce).to.equal(true);
-          expect(parentNodeIsValidStub.withArgs(body).calledOnce).to.equal(true);
+          expect(parentNodeIsValidStub.withArgs(mockTreeWalker.currentNode).calledOnce).to.equal(true);
         });
 
         it('should get the next result', () => {
           parentNodeIsValidStub.returns(false);
-          testClickAndRoll.locateAndFormatResults(body, []);
-          expect(nextResultStub.calledTwice).to.equal(true);
+          testResultSearch.mapHitsToResultNodes('', hits);
+          expect(nextHitStub.calledTwice).to.equal(true);
         });
 
         describe('if the parent node is valid', () => {
 
           it('should highlight the result', () => {
             parentNodeIsValidStub.returns(true);
-            testClickAndRoll.locateAndFormatResults(body, []);
-            expect(highlightResultStub.calledOnce).to.equal(true);
-            expect(highlightResultStub.withArgs({index: 11, name: 'LeBron James'}, body, 0).calledOnce).to.equal(true);
+            testResultSearch.mapHitsToResultNodes('', hits);
+            expect(createResultNodeStub.calledOnce).to.equal(true);
+            expect(createResultNodeStub.withArgs(hit, mockTreeWalker.currentNode, 0).calledOnce).to.equal(true);
           });
 
         });
@@ -319,10 +362,78 @@ describe('Content Scripts', () => {
 
           it('should not highlight the result', () => {
             parentNodeIsValidStub.returns(false);
-            testClickAndRoll.locateAndFormatResults(body, []);
-            expect(highlightResultStub.notCalled).to.equal(true);
+            testResultSearch.mapHitsToResultNodes('', hits);
+            expect(createResultNodeStub.notCalled).to.equal(true);
           });
 
+        });
+
+      });
+
+    });
+
+    describe('parentNodeIsValid', () => {
+
+      it('should return false if parent node is script', () => {
+        const currentNode = {
+          parentNode: {
+            nodeName: 'SCRIPT'
+          }
+        };
+        expect(testResultSearch.parentNodeIsValid(currentNode)).to.equal(false);
+      });
+
+      it('should return false if parent node is style', () => {
+        const currentNode = {
+          parentNode: {
+            nodeName: 'STYLE'
+          }
+        };
+        expect(testResultSearch.parentNodeIsValid(currentNode)).to.equal(false);
+      });
+
+      it('should return true for other parent node types', () => {
+        const currentNode = {
+          parrentNode: {
+            nodeName: 'SPAN'
+          }
+        };
+        expect(testResultSearch.parentNodeIsValid(currentNode)).to.equal(true);
+      });
+
+      it('should return true if current node has no parent', () => {
+        const currentNode = {
+          parentNode: null
+        };
+        expect(testResultSearch.parentNodeIsValid(currentNode)).to.equal(true);
+      });
+
+    });
+
+    describe('createResultNode', () => {
+
+      let hit;
+
+      before(() => {
+        hit = {start: 0, end: 11, text: 'LeBronJames'};
+      });
+
+      describe('if hit is not fully contained by passed in node', () => {
+        it('should return null', () => {
+          expect(testResultSearch.createResultNode(hit, '', 1)).to.equal(null);
+        });
+
+      });
+
+      describe('if hit is fully contained by passed in node', () => {
+
+        it('should return passed in node wrapped in new span', () => {
+          const parent = document.createElement('div');
+          const textNode = document.createTextNode('LeBron James');
+          parent.appendChild(textNode);
+          const resultNode = testResultSearch.createResultNode(hit, textNode, 0);
+          console.log(resultNode);
+          expect(resultNode.outerHTML).to.equal('<span>LeBron James</span>');
         });
 
       });
